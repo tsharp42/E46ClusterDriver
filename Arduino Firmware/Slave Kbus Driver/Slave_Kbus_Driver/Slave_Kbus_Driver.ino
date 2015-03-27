d1ebe7894488bf1649bbfb10f63a95950548eae8
Terence Sharp <tsharp42@users.noreply.github.com>
2015-03-27 02:18:03 +0000

    Bugfix, Atmel locking up - Blocking in I2C Interrupt handler?

--- Arduino Firmware/Slave Kbus Driver/Slave_Kbus_Driver/Slave_Kbus_Driver.ino
+++ Arduino Firmware/Slave Kbus Driver/Slave_Kbus_Driver/Slave_Kbus_Driver.ino
@@ -2,23 +2,12 @@
 
 #define kbus Serial
 
-/*
-// Lights B1
-bool Lights_DoubleRate      = false;
-bool Lights_LeftIndicator   = false;
-bool Lights_RightIndicator  = false;
-bool Lights_RearFogs        = false;
-bool Lights_FrontFogs       = false;
-bool Lights_FullBeam        = false;
-
-// Lights B2
-bool Car_Image = true;
-*/
-
 #define I2C_ADDRESS 0x80
 
 byte LightByte1 = 0x00;
 byte LightByte2 = 0x00;
+
+bool sendFrame = false;
 
 void setup() {
   // KBus is 9600 8E1
@@ -31,59 +20,19 @@ void setup() {
 void receiveEvent(int howMany)
 {
   // Lights
-  byte mes1[] = {0xD0, 0x08, 0xBF, 0x5B, Wire.read(), 0x00, 0x00, Wire.read(), 0x00, 0x58, 0x00};
-  sendKbus(mes1);
-  
-  delay(500);
-  
-  // Time
-  //byte mes2[] = {0x3B, 0x05, 0x80, 0x40, 0x01, 0x0C, 0x3B, 0x00};
-  
-  byte mes2[] = {0x3B, 0x05, 0x80, 0x40, 0x01, Wire.read(), Wire.read(), 0x00};
-  sendKbus(mes2);
+  LightByte1 = Wire.read();
+  LightByte2 = Wire.read();
+  sendFrame = true;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // Light Messages
-  // LIGHTS:
-  // BM = 	| Double Rate | Rind | Left | RearFogs | FrontFogs | FullBeam | ? | ? |
-  // BM2 = 	| CarImage | ? | LsideLight | RSideLight | Rr-L-Sidelight | Rr-R-Sidelight | ? | ? |
-  // D0 08 BF 5B _BM_ 00 00 _BM2_ 00 58 <CHK>
-  /*
-  byte LightByte1 = 0x00;
-  byte LightByte2 = 0x00;
   
-  // Byte 1
-  if(Lights_DoubleRate)
-    LightByte1 = LightByte1 | 0x80;
-  
-  if(Lights_RightIndicator)
-    LightByte1 = LightByte1 | 0x40;
-   
-  if(Lights_LeftIndicator)
-    LightByte1 = LightByte1 | 0x20;
-    
-  if(Lights_RearFogs)
-    LightByte1 = LightByte1 | 0x10;
-    
-  if(Lights_FrontFogs)
-    LightByte1 = LightByte1 | 0x08;
-    
-  if(Lights_FullBeam)
-    LightByte1 = LightByte1 | 0x04; 
-    
-  // Byte 2
-  if(Car_Image)
-    LightByte2 = LightByte2 | 0x80; 
-    
-  byte mes1[] = {0xD0, 0x08, 0xBF, 0x5B, LightByte1, 0x00, 0x00, LightByte2, 0x00, 0x58, 0x00};
-  sendKbus(mes1);
-  
-  
-  delay(5000);
-  */
+  if(sendFrame)
+  {
+    byte mes1[] = {0xD0, 0x08, 0xBF, 0x5B, LightByte1, 0x00, 0x00, LightByte2, 0x00, 0x58, 0x00};
+    sendKbus(mes1);
+    sendFrame = false; 
+  }
 }
 
 
